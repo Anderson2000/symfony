11ec67306aae97df9e7cd0a9dc296b82bfb7bb78
Andrew <constructors2000@gmail.com>
2019-12-27 16:23:19 +0200

    controller and routing

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,8 +3,21 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TestController
+class TestController extends AbstractController
 {
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
 
+        return $this->render('test/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
 }
