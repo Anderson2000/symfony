01c3803ae81adffa73f368f45fca71dafc5dc27c
Andrew <constructors2000@gmail.com>
2019-12-28 10:49:04 +0200

    update profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,51 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UpdateProfileForm;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class UserController extends AbstractController
 {
+
     /**
      * @Route("/profile", name="app_homepage")
      */
-    public function index(){
-        return $this->render('account/index.html.twig');
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator){
+
+        $user = $this->getUser();
+        $form = $this->createForm(UpdateProfileForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+        }
+
+        return $this->render('account/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
