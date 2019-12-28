5f29b1681c65f21c18def1254c0d6e80a4f5258b
Andrew <constructors2000@gmail.com>
2019-12-28 17:41:33 +0200

    finish

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WelcomeController  extends AbstractController
+{
+    /**
+     * @Route("/", name="app_welcome")
+     */
+    public function index(){
+        return $this->render('welcome.html.twig');
+    }
+}
